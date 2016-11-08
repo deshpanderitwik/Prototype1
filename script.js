decb6b36db8e09d63f4485c713d04deb7b461352
deshpanderitwik <deshpande.ritwik@gmail.com>
2016-11-08 08:33:58 -0500

    Got a few intervals working

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var rArray = [];
 var gArray = [];
 var bArray = [];
 
-var c, cSharp, d;
+var c, cSharp, d, dSharp, noteE, f, g;
 
-var osc1, osc2;
+var osc1, osc2, filter;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,16 +18,22 @@ function setup() {
     systems[i] = new ParticleSystem(createVector(j, height/2 + 200));
   }
 
+  filter = new p5.Filter();
+
   osc1 = new p5.Oscillator();
   osc1.setType('square');
   osc1.freq(240);
   osc1.amp(0);
+  osc1.disconnect();
+  osc1.connect(filter);
   osc1.start();
 
   osc2 = new p5.Oscillator();
   osc2.setType('square');
   osc2.freq(240);
   osc2.amp(0);
+  osc2.disconnect();
+  osc2.connect(filter);
   osc2.start();
 }
 
@@ -45,23 +51,47 @@ function draw() {
     if(c == true){
       moveY[0] = height/2;
       osc1.freq(130.813);
-      osc1.amp(0.5, 0.05);
-    }
+      osc1.amp(0.05, 0.05);
+    };
 
     if(cSharp == true) {
       moveY[1] = height/2;
       osc1.freq(138.591);
-      osc1.amp(0.5, 0.05);
-    }
+      osc1.amp(0.05, 0.05);
+    };
 
     if(d == true) {
       moveY[2] = height/2;
       osc1.freq(146.832);
-      osc1.amp(0.5, 0.05);
-    }
+      osc1.amp(0.05, 0.05);
+    };
+
+    if(dSharp == true) {
+      moveY[3] = height/2;
+      osc1.freq(155.563);
+      osc1.amp(0.05, 0.05);
+    };
+
+    if(noteE == true) {
+      moveY[4] = height/2;
+      osc1.freq(164.814);
+      osc1.amp(0.05, 0.05);
+    };
+
+    if(f == true) {
+      moveY[5] = height/2;
+      osc1.freq(174.614);
+      osc1.amp(0.05, 0.05);
+    };
+
+    if(g == true) {
+      moveY[7] = height/2;
+      osc1.freq(195.998);
+      osc1.amp(0.05, 0.05);
+    };
 
     if(c == true && cSharp == true){
-      moveX[0] = - 200;
+      moveX[0] = -200;
       moveX[1] = width/12 * 4;
 
       rArray[0] = 231;
@@ -74,14 +104,14 @@ function draw() {
       bArray[1] = 60;
 
       osc1.freq(130.813);
-      osc1.amp(0.5, 0.05);
+      osc1.amp(0.05, 0.05);
       osc2.freq(138.591);
-      osc2.amp(0.5, 0.05);
+      osc2.amp(0.05, 0.05);
     }
 
     if(cSharp == true && d == true){
-      moveX[1] = - 200;
-      moveX[2] = width/12 * 4;
+      moveX[1] = -100;
+      moveX[2] = width/12 * 5;
 
       rArray[1] = 231;
       rArray[2] = 231;
@@ -93,14 +123,14 @@ function draw() {
       bArray[2] = 60;
 
       osc1.freq(138.591);
-      osc1.amp(0.5, 0.05);
+      osc1.amp(0.05, 0.05);
       osc2.freq(146.832);
-      osc2.amp(0.5, 0.05);
+      osc2.amp(0.05, 0.05);
     }
 
     if(c == true && d == true){
-      moveX[0] = width/12 * 2;
-      moveX[2] = width/12 * 2;
+      moveX[0] = 0;
+      moveX[2] = width/12 * 3;
 
       rArray[0] = 46;
       rArray[2] = 46;
@@ -112,9 +142,47 @@ function draw() {
       bArray[2] = 113;
 
       osc1.freq(130.813);
-      osc1.amp(0.5, 0.05);
+      osc1.amp(0.05, 0.05);
       osc2.freq(146.832);
-      osc2.amp(0.5, 0.05);
+      osc2.amp(0.05, 0.05);
+    }
+
+    if(noteE == true && f == true){
+      moveX[4] = 300;
+      moveX[5] = width/12 * 7;
+
+      rArray[4] = 231;
+      rArray[5] = 231;
+
+      gArray[4] = 76;
+      gArray[5] = 76;
+
+      bArray[4] = 60;
+      bArray[5] = 60;
+
+      osc1.freq(164.814);
+      osc1.amp(0.05, 0.05);
+      osc2.freq(174.614);
+      osc2.amp(0.05, 0.05);
+    }
+
+    if(c == true && g == true){
+      moveX[0] = width/2 - 300;
+      moveX[7] = width/12 * 5;
+
+      rArray[0] = 46;
+      rArray[7] = 46;
+
+      gArray[0] = 204;
+      gArray[7] = 204;
+
+      bArray[0] = 113;
+      bArray[7] = 113;
+
+      osc1.freq(130.813);
+      osc1.amp(0.05, 0.05);
+      osc2.freq(195.998);
+      osc2.amp(0.05, 0.05);
     }
 
     systems[i].addParticle();
@@ -208,4 +276,28 @@ keyboardJS.bind('s', function(e) {
   d = true;
 }, function(e) {
   d = false;
+});
+
+keyboardJS.bind('e', function(e) {
+  dSharp = true;
+}, function(e) {
+  dSharp = false;
+});
+
+keyboardJS.bind('d', function(e) {
+  noteE = true;
+}, function(e) {
+  noteE = false;
+});
+
+keyboardJS.bind('f', function(e) {
+  f = true;
+}, function(e) {
+  f = false;
+});
+
+keyboardJS.bind('g', function(e) {
+  g = true;
+}, function(e) {
+  g = false;
 });
