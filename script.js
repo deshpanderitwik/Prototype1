60baf4315fd938ec4b9dcaa0d69d3c19d1dfa2c3
deshpanderitwik <deshpande.ritwik@gmail.com>
2016-11-08 03:42:31 -0500

    Added attraction and repelling

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var systems = [];
+
 var moveX = [];
 var moveY = [];
-var c, cSharp;
+
+var rArray = [];
+var gArray = [];
+var bArray = [];
+
+var c, cSharp, d;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,34 +20,65 @@ function setup() {
 function draw() {
   background(0);
 
-  for (i=0, j=(width/12)/2; i < 12; i++, j = j + width/12) {
+  for (i=0, j=(width/12)/2; i < systems.length; i++, j = j + width/12) {
     moveX[i] = j;
     moveY[i] = height/2 + 200;
-  }
 
-  for (i=0, j=(width/12)/2; i < systems.length; i++, j = j + width/12) {
-    systems[i].addParticle();
-    systems[i].run(moveX[i], moveY[i]);
+    rArray[i] = 255;
+    gArray[i] = 255;
+    bArray[i] = 255;
 
-    if(mouseIsPressed){
-      moveX[1] = width/2;
+    if(c == true){
+      moveY[0] = height/2;
+    }
+
+    if(cSharp == true) {
       moveY[1] = height/2;
     }
+
+    if(d == true) {
+      moveY[2] = height/2;
+    }
+
+    if(c == true && cSharp == true){
+      moveX[0] = - 200;
+      moveX[1] = width/12 * 4;
+      rArray[0] = 231;
+      rArray[1] = 231;
+      gArray[0] = 76;
+      gArray[1] = 76;
+      bArray[0] = 60;
+      bArray[1] = 60;
+    }
+
+    if(c == true && d == true){
+      moveX[0] = width/12 * 2;
+      moveX[2] = width/12 * 2;
+      rArray[0] = 46;
+      rArray[2] = 46;
+      gArray[0] = 204;
+      gArray[2] = 204;
+      bArray[0] = 113;
+      bArray[2] = 113;
+    }
+
+    systems[i].addParticle();
+    systems[i].run(moveX[i], moveY[i], rArray[i], gArray[i], bArray[i]);
   }
 }
 
 // Particle
 var Particle = function(location) {
   this.location = location.copy();
-  this.velocity = createVector(random(-2, 2), random(-2, 0));
+  this.velocity = createVector(random(-5, 5), random(-5, 0));
   this.acceleration = createVector(0,0);
   this.lifespan = 255.0;
   this.lifespanInvert = 0;
 };
 
-Particle.prototype.run = function(moveX, moveY) {
+Particle.prototype.run = function(moveX, moveY, rVal, gVal, bVal) {
   this.update(moveX, moveY);
-  this.display();
+  this.display(rVal, gVal, bVal);
 };
 
 Particle.prototype.update = function(moveX, moveY) {
@@ -60,9 +97,9 @@ Particle.prototype.update = function(moveX, moveY) {
   this.lifespanInvert += 2;
 };
 
-Particle.prototype.display = function() {
+Particle.prototype.display = function(rVal, gVal, bVal) {
   noStroke();
-  fill(this.location.x/4, 200, this.lifespan, this.lifespan);
+  fill(rVal, gVal, bVal, this.lifespan);
   ellipse(this.location.x, this.location.y,this.lifespanInvert/6,this.lifespanInvert/6);
 };
 
@@ -84,10 +121,10 @@ ParticleSystem.prototype.addParticle = function() {
   this.particles.push(new Particle(this.origin, this.pointer));
 };
 
-ParticleSystem.prototype.run = function(moveX, moveY) {
+ParticleSystem.prototype.run = function(moveX, moveY, rVal, gVal, bVal) {
   for (var i = this.particles.length-1; i >= 0; i--) {
     var p = this.particles[i];
-    p.run(moveX, moveY);
+    p.run(moveX, moveY, rVal, gVal, bVal);
     if (p.isDead()) {
       this.particles.splice(i, 1);
     }
@@ -105,4 +142,10 @@ keyboardJS.bind('w', function(e) {
   cSharp = true;
 }, function(e) {
   cSharp = false;
+});
+
+keyboardJS.bind('s', function(e) {
+  d = true;
+}, function(e) {
+  d = false;
 });
